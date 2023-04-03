03541fb8e65b74d18a4f223841fb6a47058e6ef5
Abhishek-Manoj <abhishekmanojofficial@gmail.com>
2023-04-03 14:58:12 +0530

    Assessment 2 - Developmental Questions

---
+++ DLTE-Assessment/DLTE-Assessment-002-RestApi/src/test/java/dlte/assessment/rest/api/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dlte.assessment.rest.api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
